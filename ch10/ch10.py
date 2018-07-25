6b72a7d439cba2a05c7d08d9b9858bab1e625968
Marlen <mbissaliyev@gmail.com>
2018-07-26 03:06:22 +0600

    Ch10

--- ch10/ch10.py
+++ ch10/ch10.py
@@ -21,3 +21,22 @@ d = np.diag(M)
 # step 2 - create diagonal matrix from vector
 D = np.diag(d)
 print("Diagonal", D)
+
+# Identity Matrix
+I = np.identity(3)
+print(I)
+
+# Orthonogal Matrix
+# step 1 - define matrix
+Q = np.array([[1, 0],
+              [0, -1]])
+
+print(Q)
+
+# inverse equivalence
+V = np.linalg.inv(Q)
+print(V)
+print(Q.T)
+
+# identify equivalence
+I = Q.dot(Q.T)
