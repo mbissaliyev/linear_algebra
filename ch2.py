cbee5d81200d9d376cd1a10f209777ec1c06135c
Marlen <mbissaliyev@gmail.com>
2018-07-26 00:40:42 +0600

    Init

---
+++ ch2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#creating 1D array
+a = [1,2,3]
+
+
+a = np.array(a)
+print(a)
+print(a.shape)
+
+#importing csv
+import pandas as pd
+
+data = pd.read_csv("data.csv")
+
+#converting to 2D array
+data = np.array(data)
+print(data)
+
