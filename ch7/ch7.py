1c1b26aea30958e9f422b9b4f031f0b90374d7c4
Marlen <mbissaliyev@gmail.com>
2018-07-26 01:24:02 +0600

    Ch7

--- ch7/ch7.py
+++ ch7/ch7.py
@@ -8,3 +8,42 @@
 #     (v4)
 #     (v5)
 
+
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a)
+
+b = np.array([1, 2, 3])
+print(b)
+
+# addition
+c = a + b
+print(c)
+
+# substraction
+d = a - b
+print(d)
+
+# multiplication
+v = a * b
+print(v)
+
+# division
+dd = a / b
+print(dd)
+
+# vector scalar multiplication
+a = np.array([1, 2, 3])
+print(a)
+
+b = np.array([3, 4, 5])
+print(b)
+
+c = a.dot(b)
+
+# c = a1*b1 + a2*b2 + a3*b3
+# c = 3 + 8 + 15
+
+
+print(c)
