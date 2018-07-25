38cbdaf627aba39145b3cbb79529d8243b146b80
Marlen <mbissaliyev@gmail.com>
2018-07-26 02:37:31 +0600

    Ch8

--- ch8/ch8.py
+++ ch8/ch8.py
@@ -3,16 +3,21 @@
 # 1 Vector Norms
 import numpy as np
 
-#define vector
-a = np.array([1,2,3])
+# define vector
+a = np.array([1, 2, 3])
 print(a)
 
 # calculate L-1 NORM = L1 = A[0] + A[1] + A[2]
 
-l1 = np.linalg.norm(a,1)
+l1 = np.linalg.norm(a, 1)
 print(l1)
 
 # L-2 Norm = L2 = SQRT(A[0]^2 + A[1]^2 + A[2]^2)
 l2 = np.linalg.norm(a)
 print(l2)
+
+# Vector MAX-NORM = max(A[0],A[1] A[2])
+maxnorm = np.linalg.norm(a, np.inf)
+print(maxnorm)
+
 
