881411f47d78a7fec5e22bc13340bc80bbe87e51
Marlen <mbissaliyev@gmail.com>
2018-07-26 01:02:27 +0600

    Init

--- ch6/ch6.py
+++ ch6/ch6.py
@@ -1,26 +1,37 @@
-#chapter 6 - numpy array broadcasting
+# chapter 6 - numpy array broadcasting
 
 import numpy as np
 
-a = [1,2,3]
-b = [1,2,3]
-c = a+b
+a = [1, 2, 3]
+b = [1, 2, 3]
+c = a + b
 print(c)
 
+# a = [a1, a2, a3]
+# b = [b1, b2, b3]
+# c = a + b
 
-#a = [a1, a2, a3]
-#b = [b1, b2, b3]
-#c = a + b
 
-
-#broadcast scalar array
-a = np.array([1,2,3])
+# broadcast scalar array
+a = np.array([1, 2, 3])
 print(a)
 
 b = 4
-#print(b)
+# print(b)
 
 c = a + b
 print(c)
 
+# broadcast to 2D array
 
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+print("Array A:", A)
+
+# define scalar value
+
+b = 2
+print("Scalar b:", b)
+c = A + b
+print("Array C:", c)
