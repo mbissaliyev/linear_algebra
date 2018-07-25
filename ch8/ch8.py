2d6d5778342101245f6cff492a523d96118b9499
Marlen <mbissaliyev@gmail.com>
2018-07-26 02:34:11 +0600

    Ch8

---
+++ ch8/ch8.py
@@ -0,0 +1,18 @@
+# Vector Norms - vector lengs and magnitutdes
+
+# 1 Vector Norms
+import numpy as np
+
+#define vector
+a = np.array([1,2,3])
+print(a)
+
+# calculate L-1 NORM = L1 = A[0] + A[1] + A[2]
+
+l1 = np.linalg.norm(a,1)
+print(l1)
+
+# L-2 Norm = L2 = SQRT(A[0]^2 + A[1]^2 + A[2]^2)
+l2 = np.linalg.norm(a)
+print(l2)
+
