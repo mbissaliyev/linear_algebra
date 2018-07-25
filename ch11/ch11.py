2dfa3748928424f6c3bace32e3a4b356b5ae582e
Marlen <mbissaliyev@gmail.com>
2018-07-26 03:19:48 +0600

    Ch10

--- ch11/ch11.py
+++ ch11/ch11.py
@@ -31,28 +31,24 @@ B = np.trace(A)
 print(B)
 
 # Determinant - scalar representation of the volument of the matrix
-A = np.array([[1,2,3],
-              [4,5,6],
-              [7,8,9]])
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
 print(A)
 B = np.linalg.det(A)
 print(B)
 
-
 # Rank - linearly independent rows
 
-A = np.array([1,2,3])
+A = np.array([1, 2, 3])
 vr1 = np.linalg.matrix_rank(A)
 print(vr1)
 
-B = np.array([0,0,0,0])
+B = np.array([0, 0, 0, 0])
 vr2 = np.linalg.matrix_rank(B)
 print(vr2)
 
-A = np.array([[1,2],
-              [3,4]])
+A = np.array([[1, 2],
+              [3, 4]])
 vr1 = np.linalg.matrix_rank(A)
 print(vr1)
-
-
-
