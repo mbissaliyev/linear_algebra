44670fbddcda55139dcb523fcff4b682ab9ebd63
marlen <mbissaliyev@gmail.com>
2018-08-06 14:50:33 +0600

    ch14

---
+++ ch14/ch14.py
@@ -0,0 +1,42 @@
+# Chapter 14 matrix decomposition
+
+import numpy as np
+from scipy.linalg import lu
+from scipy.linalg import qr
+
+
+A = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+
+])
+
+print(A)
+
+P, L, U = lu(A)
+print(P)
+print(L)
+print(U)
+
+# reconstruct
+B = P.dot(L).dot(U)
+print(B)
+
+# QR decomposition
+
+A = np.array([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+])
+
+print(A)
+
+Q, R = qr(A)
+print(Q)
+print(R)
+
+#reconstruct
+B = Q.dot(R)
+print(B)
