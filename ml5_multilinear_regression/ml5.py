0a703be64631376ee8f1028464e4c0e7bd864a2f
marlen <mbissaliyev@gmail.com>
2018-08-07 19:15:18 +0600

    Machine Learning - Multilinear Regression

--- ml5_multilinear_regression/ml5.py
+++ ml5_multilinear_regression/ml5.py
@@ -15,9 +15,7 @@ A = np.array(dataset)
 # STATE = dataset.iloc[:, 3:4].values
 
 X = dataset.iloc[:, :-1].values
-y =dataset.iloc[:, 4].values
-
-print(X[:,3])
+y = dataset.iloc[:, 4].values
 
 # encoding states
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -28,4 +26,27 @@ onehotencoder = OneHotEncoder(categorical_features=[3])
 X = onehotencoder.fit_transform(X).toarray()
 print(X)
 
+# avoiding dummy variable trap
+X = X[:, 1:]
 
+# split Test and Train set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# fit multiple regression to the training set
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# predicting the test set results
+y_pred = regressor.predict(X_test)
+# y_pred = np.reshape(y_pred,(10,1))
+
+
+# backward elimination
+import statsmodels.formula.api as sm
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
