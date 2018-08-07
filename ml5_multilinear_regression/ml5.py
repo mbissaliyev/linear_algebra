94a1df30c6fe18d926711ab4d699f290f24bd057
marlen <mbissaliyev@gmail.com>
2018-08-07 19:33:58 +0600

    Machine Learning - Multilinear Regression

--- ml5_multilinear_regression/ml5.py
+++ ml5_multilinear_regression/ml5.py
@@ -50,3 +50,17 @@ import statsmodels.formula.api as sm
 
 X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 1,  3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
