59e4d567f1b3e03d1c5c44ec1b6402e6eec53b93
Marlen <mbissaliyev@gmail.com>
2018-08-07 23:40:37 +0600

    Ch4

---
+++ stats4/ch4.py
@@ -0,0 +1,20 @@
+#chapter 4 Gaussian Distribution
+
+import numpy as np
+from matplotlib import pyplot
+from scipy.stats import norm
+
+X_axis = np.arange(-3,3,0.001)
+
+y_axis = norm.pdf(X_axis,0,1)
+
+#pyplot.plot(X_axis,y_axis)
+#pyplot.show()
+
+# test dataset
+
+np.random.seed(1)
+data = 5 * np.random.rand(10000) + 50
+
+pyplot.hist(data, bins=100)
+pyplot.show()
